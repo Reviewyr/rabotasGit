6a41baac992651c83452347ea04e1afe72f25228
rezvuuhin.yarik@yandex.ru <pl9017@PL1.EDU>
2024-02-22 09:26:26 +0500

    НОВЫй ПроекТ

--- rabotasGit/rabotasGit/Form1.Designer.cs
+++ rabotasGit/rabotasGit/Form1.Designer.cs
@@ -29,23 +29,47 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(247, 201);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(297, 125);
+            this.button1.Size = new System.Drawing.Size(149, 50);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 49);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 123);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 49);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(181, 205);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,5 +80,7 @@
         #endregion
 
         private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
--- rabotasGit/rabotasGit/Form1.cs
+++ rabotasGit/rabotasGit/Form1.cs
@@ -17,5 +17,29 @@ namespace rabotasGit
             newForm.Controls.Add(textBox);
             newForm.Show();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form redForm = new Form();
+            redForm.Size = new Size(200, 200);
+            redForm.BackColor = Color.Red;
+            redForm.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form squareForm = new Form();
+            squareForm.Size = new Size(200, 200);
+            Graphics graphics = squareForm.CreateGraphics();
+
+            int sideLength = Math.Min(squareForm.Width, squareForm.Height) / 2; // Размер стороны квадрата равен минимальному размеру окна
+            Pen pen = new Pen(Color.Black, 2); // Цвет линии и толщина
+
+            Point startPoint = new Point((squareForm.ClientRectangle.Width - sideLength) / 2, (squareForm.ClientRectangle.Height - sideLength) / 2); // Точка старта рисуния
+
+            graphics.Clear(Color.White); // Очищаем фон перед рисовкой
+            graphics.DrawRectangle(pen, startPoint.X, startPoint.Y, sideLength, sideLength); // Рисуем квадрат
+            squareForm.ShowDialog();
+        }
     }
 }
